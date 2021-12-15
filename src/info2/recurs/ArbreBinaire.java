75b1753c20999b114b5b0475b3093d5d2f81096f
Corentin DEPRECQ <corentin.deprecq@iut-rodez.fr>
2021-12-15 09:45:15 +0100

    Commit changes de ArbreBinaire et Noeud

--- src/info2/recurs/ArbreBinaire.java
+++ src/info2/recurs/ArbreBinaire.java
@@ -1,14 +1,169 @@
+/*
+ * Classe qui représente un arbre binaire générique            12/17
+ * ArbreBinaire.java
+ */
+
 package info2.recurs;
 
-public class ArbreBinaire {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-    private Noeud noeudRacine;
+/**
+ * Classe qui représente un arbre binaire de recherche qui contient des valeurs
+ * d'un type T. L'arbre ne peut pas contenir de valeur en double. Il peut être
+ * vide.
+ * @param <T> type paramètre de généricité : le type des éléments de l'arbre. Les
+ *            objets de ce type doivent être "comparables" (utilisation de la
+ *            méthode compareTo)
+ * @author INFO2
+ */
+public class ArbreBinaire<T extends Comparable<T>> implements Serializable {
 
+
+
+    /**
+     * Noeud racine de l'arbre binaire (peut être égal à null)
+     */
+    private Noeud<T> racine;
+
+    /**
+     * Constructeur par défaut : l'arbre binaire est vide
+     */
     public ArbreBinaire() {
-        this.noeudRacine = null;
+        racine = null;          // pas de noeud racine
     }
 
-    public boolean estVide() {
-        return this.noeudRacine == null;
+    /**
+     * Insère la valeur argument dans l'arbre courant
+     * @param aInserer entier à insérer
+     * @return un booléan égal à vrai ssi l'insertion est possible, c'est-à-dire
+     *         si la valeur à insérer n'est pas déjà présente
+     */
+    public boolean inserer(T aInserer) {
+        if (racine == null) {
+
+            // l'arbre est vide : insertion sur un nouveau noeud
+            racine = new Noeud<T>(aInserer);
+            return true;
+        }
+
+        // sinon : insertion à partir de la racine
+        return racine.inserer(aInserer);
     }
+
+    /**
+     * Détermine si la valeur argument est présente dans l'arbre binaire
+     * @param aChercher valeur à rechercher dans l'arbre
+     * @return un booléen égal à vrai ssi la valeur est présente dans l'arbre
+     */
+    public boolean estPresente(T aChercher) {
+        if (racine == null) {
+
+            // l'arbre est vide : la valeur n'est pas présente
+            return false;
+        }
+
+        // sinon : recherche à partir du noeud racine
+        return racine.estPresente(aChercher);
+    }
+
+
+    /**
+     * Affiche les valeurs contenues dans l'arbre dans l'ordre croissant
+     */
+    public void afficheOrdreCroissant() {
+        if (racine == null) {
+            System.out.println("L'arbre est vide.");
+        } else {
+
+            /*
+             * appel à afficheOrdreCroissant sur la racine pour lancer les appels
+             * récursifs
+             */
+            racine.afficheOrdreCroissant();
+        }
+    }
+
+
+    /**
+     * Affiche les valeurs contenues dans l'arbre binaire. La racine est
+     * affichée sur la colonne de gauche, les valeurs des noeuds enfants sont
+     * affichées sur la colonne suivante et ainsi de suite. Le nombre d'espaces
+     * de séparation entre les colonnes est égal à DECALAGE.
+     */
+    public void afficheArbreNiveau() {
+        if (racine == null) {
+            System.out.println("L'arbre est vide.");
+        } else {
+
+            /*
+             * appel à afficheCroissant sur la racine pour lancer les appels
+             * récursifs : la racine sera affichée à gauche de l'écran
+             */
+            racine.afficheArbreNiveau(0);
+        }
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+// * Supprime la valeur argument si elle est présente et située sur une
+// * feuille
+// * @param valeur à supprimer
+// * @return un booléen égal à vrai ssi la suppression a été effectuée
+// */
+// public boolean supprimeSiFeuille ( T valeur ) {
+// if ( racine == null ) {
+// return false ;
+// }
+// if ( racine.getValeur() == valeur ) {
+// if ( racine.getArbreDroit() == null && racine.getArbreGauche() == null )
+// {
+// // la valeur est présente sur une feuill
+// racine = null ;
+// return true ;
+// } else {
+// // valeur présente mais pas sur une feuille
+// return false ;
+// }
+// }
+// return supprimeSiFeuille ( racine , valeur ) ;
+// }
+
+// private boolean supprimeSiFeuille ( Noeud<T> parent , T valeur ) {
+// if ( valeur.compareTo(parent.getValeur()) <0 ) {
+// if ( parent.getArbreGauche().getValeur() == valeur ) {
+// if ( parent.getArbreGauche().getArbreGauche() == null &&
+// parent.getArbreGauche().getArbreDroit() == null ) {
+// // valeur sur une feuill
+// parent.
+// }
+// }
+// }
--- src/info2/recurs/Noeud.java
+++ src/info2/recurs/Noeud.java
@@ -1,125 +1,212 @@
+
+/*
+ * Noeud d'un arbre binaire                    12/17
+ * Noeud.java
+ */
 package info2.recurs;
+import java.io.Serializable;
+import java.util.ArrayList;
 
-public class Noeud <T extends Comparable<T>> {
+/**
+ * Classe que représnte un noeud dans un arbre binaire de recherche, sans valeur
+ * en double. Chaque noeud contient une valeur d'un type T, un sous-arbre gauche
+ * éventuellement vide, et un sous-arbre droit éventuellement vide. Le
+ * sous-arbre gauche ne contient que des valeurs inférieures à la valeur du
+ * noeud courant. Le sous-arbre droit ne contient que des valeurs supérieures à
+ * la valeur du noeud couant.
+ *
+ * @param <T> type paramètre de généricité : le type des éléments de l'arbre. Les
+ *            objets de ce type doivent être "comparables" (utilisation de la
+ *            méthode compareTo)
+ * @author INFO2
+ */
+public class Noeud<T extends Comparable<T>> implements Serializable {
 
-    /* Valeur du Noeud */
+
+    /**
+     * Constante égale au nombre d'espaces d'indentation à laisser lors de
+     * l'affichage des différents niveaux de l'arbre
+     */
+    private static final int DECALAGE = 5;
+
+
+    /**
+     * Valeur du noeud
+     */
     private T valeur;
 
-    /* Référence vers le noeud racine vers le sous arbre gauche */
-    private Noeud<T> refGauche;
-
-    /* Référence vers le noeud racine vers le sous arbre droit */
-    private Noeud<T> refDroit;
+    /**
+     * sous-arbre gauche du noeud courant : contient des valeurs inférieures à
+     * celle du noeud courant
+     */
+    private Noeud<T> arbreGauche;
 
     /**
-     * Initialise un noeud avec des valeurs des sous arbres non définis
-     * @param valeur valeur du noeud
+     * Sous-arbre droit du noeud courant : contient des valeurs supérieures à
+     * celle du noeud courant
      */
-    public Noeud(T valeur) {
-        this.valeur = valeur;
-        this.refDroit = null;
-        this.refGauche = null;
+    private Noeud<T> arbreDroit;
+
+
+    /**
+     * Constructeur avec la valeur du noeud à créer. Le noeud créé n'aura pas de
+     * descendant.
+     * @param valeurNoeud contient la valeur du noeud
+     */
+    public Noeud(T valeurNoeud) {
+        valeur = valeurNoeud;
+        arbreGauche = arbreDroit = null;
     }
 
     /**
-     * Méthode qui détermine si la valeur argument est présente dans l'arbre
-     * @param valeur valeur a rechercher
-     * @return false si n'existe pas et true si elle existe
-     */
-    public boolean estPresente(T valeur) {
-
-        if (valeur.compareTo(this.valeur) == 0) {
-            return true;
-        }
-
-        if (valeur.compareTo(this.valeur) > 0 && this.refDroit != null) {
-            return this.refDroit.estPresente(valeur);
-        }
-
-        if (valeur.compareTo(this.valeur) < 0 && this.refGauche != null) {
-            return this.refGauche.estPresente(valeur);
-        }
-
-        return false;
-    }
-
-    /**
-     * Insère une valeur dans l'arbre
-     * @param valeur valeur à insérer
-     * @return true si ajouté et false sinon
-     */
-    public boolean inserer(T valeur) {
-
-        if (valeur.compareTo(this.valeur) > 0 && this.refDroit == null) {
-            this.refDroit = new Noeud<>(valeur);
-            return true;
-        }
-
-        if (valeur.compareTo(this.valeur) > 0 && this.refDroit != null) {
-            return this.refDroit.inserer(valeur);
-        }
-
-        if (valeur.compareTo(this.valeur) < 0 && this.refGauche == null) {
-            this.refDroit = new Noeud<>(valeur);
-            return true;
-        }
-
-        if (valeur.compareTo(this.valeur) < 0 && this.refGauche != null) {
-            return this.refGauche.inserer(valeur);
-        }
-
-        return false;
-    }
-
-    /**
-     * @return Renvoie la valeur du noeud courant
+     * Accesseur sur la valeur du noeud courant
+     * @return la valeur contenue dans le noeud courant
      */
     public T getValeur() {
         return valeur;
     }
 
     /**
-     * @return Renvoie la valeur du noeud Gauche
+     * Accesseur sur le sous-arbre gauche
+     * @return le sous-arbre gauche du noeud courant
      */
-    public Noeud<T> getRefGauche() {
-        return refGauche;
+    public Noeud<T> getArbreGauche() {
+        return arbreGauche;
     }
 
     /**
-     * @return Renvoie la valeur du noeud Droite
+     * Accesseur sur le sous-arbre droit
+     * @return le sous-arbre droit du noeud courant
      */
-    public Noeud<T> getRefDroit() {
-        return refDroit;
+    public Noeud<T> getArbreDroit() {
+        return arbreDroit;
     }
 
-    public void afficher() {
 
-        if (this.refGauche != null) {
-            this.refGauche.afficher();
-        }
 
-        if (this.refDroit != null) {
-            this.refDroit.afficher();
-        }
+    /**
+     * Ajoute la valeur argument à l'arbre qui débute au noeud courant.
+     * @param aInserer
+     *            valeur à insérer à gauche si inférieure au noeud courant, à
+     *            droite si supérieure au noeud courant
+     * @return un booléen égal à vrai ssi l'insertion a pu se faire,
+     *         c'est-à-dire si la valeur à insérer n'est pas déjà présente dans
+     *         l'arbre
+     */
+    public boolean inserer(T aInserer) {
+        if (aInserer.compareTo(valeur) < 0) {
 
-        System.out.print(this.valeur);
-    }
+            // si insertion possible, elle se fera à gauche du noeud
+            if (arbreGauche == null) {
 
-    public String prefixe() {
+                // le sous-arbre gauche est vide : création d'un nouveau noeud
+                arbreGauche = new Noeud<>(aInserer);
+                return true;
+            } else {
 
-        if (this.refGauche != null) {
-            return this.refGauche.prefixe();
-        } else if (this.refDroit != null) {
-            return  this.refDroit.prefixe();
+                // appel récursif pour insérer à gauche
+                return arbreGauche.inserer(aInserer);
+            }
+        } else if (aInserer.compareTo(valeur) > 0) {
+
+            // si insertion possible, elle se fera à droite du noeud
+            if (arbreDroit == null) {
+
+                // le sous-arbre droit est vide : création d'un nouveau noeud
+                arbreDroit = new Noeud<>(aInserer);
+                return true;
+            } else {
+
+                // appel récursif pour insérer à droite
+                return arbreDroit.inserer(aInserer);
+            }
         } else {
-            return  this.valeur + "  ";
+
+            // la valeur à insérer est égale à la valeur du noeud courant
+            return false; // insertion impossible
         }
     }
 
-    public String infixe() {
-        if (this.refGauche != null) {
-            return "";
+    /**
+     * Détermine si la valeur argument est présente dans l'arbre débutant au
+     * noeud courant
+     * @param aChercher paramètre contenant la valeur à chercher
+     * @return un booléan égal à vrai ssi la valeur a été trouvée
+     */
+    public boolean estPresente(T aChercher) {
+        if (aChercher.compareTo(valeur) == 0) {
+
+            // la valeur du noeud courant est égale à celle recherchée
+            return true;
         }
-        return "";
+        if (aChercher.compareTo(valeur) < 0) {
+
+            // il faut chercher dans le sous-arbre gauche
+            return arbreGauche != null && arbreGauche.estPresente(aChercher);
+        }
+
+        /*
+         * else : on a aChercher > valeur
+         * il faut chercher dans le sous-arbre droit
+         */
+        return arbreDroit != null && arbreDroit.estPresente(aChercher);
     }
+
+
+    /**
+     * Affiche les valeurs des noeuds, à partir du noeud courant,
+     * dans l'ordre croissant
+     */
+    public void afficheOrdreCroissant() {
+
+        // on affiche si possible les valeurs situées à gauche
+        if (arbreGauche != null) {
+            arbreGauche.afficheOrdreCroissant();
+        }
+
+        // on affiche la valeur du noeud courant
+        System.out.println(valeur + "  ");
+
+        // on affiche si possible les valeurs situées à droit
+        if (arbreDroit != null) {
+            arbreDroit.afficheOrdreCroissant();
+        }
+    }
+
+
+
+    /**
+     * Affiche les valeurs contenues dans l'arbre débutant au noeud argument
+     * Chaque fois que l'on descend d'un niveau dans l'arbre, les valeurs des
+     * noeuds sont affichées (en colonne) à droite des précédentes.
+     * @param niveau niveau de profondeur du noeud courant. Cette valeur sert à calculer
+     *               sur quelle colonne il faut effectuer l'affichage
+     */
+    public void afficheArbreNiveau(int niveau) {
+
+        /*
+         * on affiche d'abord le sous-arbre gauche, avec un niveau de
+         * profondeur égal à niveau+1
+         */
+        if (arbreGauche != null) {
+            arbreGauche.afficheArbreNiveau(niveau + 1);
+        }
+
+        // on laisse des espaces avant d'afficher le noeud courant
+        for (int i = 0; i < niveau * DECALAGE; i++) {
+            System.out.print(' ');
+        }
+        System.out.println(valeur);
+
+        /*
+         * on affiche ensuite le sous-arbre droit, avec un niveau de
+         * profondeur égal à niveau+1
+         */
+        if (arbreDroit != null) {
+            arbreDroit.afficheArbreNiveau(niveau + 1);
+        }
+    }
+
+
+
 }
